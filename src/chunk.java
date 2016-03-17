a2374d021e9420cda4bd4281e2c8a3c677214a8e
ivolimasilva <ivolimasilva@outlook.com>
2016-03-17 12:29:57 +0000

    Bug solved.

--- src/chunk.java
+++ src/chunk.java
@@ -1,19 +1,18 @@
 /**
  * Created by Daniel Nunes on 17-03-2016.
  */
-public class Chunk
-{
+public class Chunk {
+
     protected String fileID;
     protected int chunkNo;
     protected int originalRep;
     protected int currentRep;
 
-
-    public Chunk(String fileID, int chunkNo, int originalRep)
-    {
+    public Chunk(String fileID, int chunkNo, int originalRep) {
         this.fileID = fileID;
         this.chunkNo = chunkNo;
         this.originalRep = originalRep;
         currentRep = originalRep;
     }
+
 }
