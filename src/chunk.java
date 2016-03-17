0b75151d45bcb90d4e009a4a6bfafcc2e70ff600
PNDaniel <daniel_nunes_21@hotmail.com>
2016-03-17 12:27:19 +0000

    This works. But no.

--- src/chunk.java
+++ src/chunk.java
@@ -1,7 +1,19 @@
 /**
  * Created by Daniel Nunes on 17-03-2016.
  */
-public class chunk
+public class Chunk
 {
+    protected String fileID;
+    protected int chunkNo;
+    protected int originalRep;
+    protected int currentRep;
 
+
+    public Chunk(String fileID, int chunkNo, int originalRep)
+    {
+        this.fileID = fileID;
+        this.chunkNo = chunkNo;
+        this.originalRep = originalRep;
+        currentRep = originalRep;
+    }
 }
--- src/dummy.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by ivoli on 17/03/2016.
- */
-public class dummy {
-}
