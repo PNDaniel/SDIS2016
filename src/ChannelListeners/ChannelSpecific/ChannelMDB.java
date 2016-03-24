24537a4e0c3e5d9079ff6db7a30eb3e4b38be583
ivolimasilva <ivolimasilva@outlook.com>
2016-03-24 17:41:29 +0000

    Added new channels classes. For real this time.

---
+++ src/ChannelListeners/ChannelSpecific/ChannelMDB.java
@@ -0,0 +1,45 @@
+package ChannelListeners.ChannelSpecific;
+
+import ChannelListeners.Channel;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+
+public class ChannelMDB extends Channel {
+
+    public ChannelMDB(InetAddress _ip, int _port) {
+        super(_ip, _port);
+    }
+
+    public void run() {
+        System.out.println("Channel MDB listening on port " + this.getPort());
+
+        // Create a buffer of bytes, which will be used to store
+        // the incoming bytes containing the information from the server.
+        // Since the message is small here, 256 bytes should be enough.
+        byte[] buf = new byte[256];
+
+        // Create a new Multicast socket (that will allow other sockets/programs
+        // to join it as well.
+        try (MulticastSocket clientSocket = new MulticastSocket(this.getPort())) {
+
+            //Joint the Multicast group.
+            clientSocket.joinGroup(this.getIp());
+
+            while (true) {
+                // Receive the information and print it.
+                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
+                clientSocket.receive(msgPacket);
+
+                String msg = new String(buf, 0, buf.length);
+                System.out.println("MDB - Message received: " + msg);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
